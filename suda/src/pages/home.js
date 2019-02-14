8a4a9ee745a6521aef9ba8cfdfc19204a5b11049
aaa <1232@qq.com>
2019-02-14 19:41:11 +0800

    首页搜索框

--- suda/src/pages/home.js
+++ suda/src/pages/home.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import '../sass/home.scss';
+import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
 
 class Home extends Component {
+      componentDidMount() {
+        this.autoFocusInst.focus();
+      }      
     render() {
       return (
         <div className="Home">
-          <h1>Hello world</h1>
+          <div className='head'>
+      <SearchBar placeholder="搜索" ref={ref => this.autoFocusInst = ref} onChange={this.onChange}/>
+      <WhiteSpace />
+          </div>
+          <div className='navtabber'>111</div>
         </div>
       );
     }
